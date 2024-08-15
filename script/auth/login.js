197904b4b8343de320619026ef946fc646e25e8a
MikeyCeeWKay <138363473+MikeyCeeWKay@users.noreply.github.com>
2024-08-15 19:21:19 +0700

    Update login.js

--- script/auth/login.js
+++ script/auth/login.js
@@ -44,7 +44,7 @@ button.addEventListener('click', async function() {
         
         localStorage.setItem('username', username);
 
-        window.location.href = "../digitalplanner/index.html";
+        window.location.href = "..";
 
     } catch (error) {
         console.error("Error signing in:", error.message);
